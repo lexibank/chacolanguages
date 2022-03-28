742f58cab055ad9fc4a08264dace6a2a44296f96
lingulist <mattis.list@lingpy.org>
2022-03-28 15:53:23 +0200

    update new files

---
+++ lexibank_chacolanguages.py
@@ -0,0 +1,112 @@
+import attr
+from pathlib import Path
+
+from pylexibank import Concept, Language, FormSpec, Lexeme
+from pylexibank.dataset import Dataset as BaseDataset
+from pylexibank.util import progressbar
+from lingpy import Wordlist
+from pyedictor import fetch
+
+from clldutils.misc import slug
+from unicodedata import normalize
+
+
+@attr.s
+class CustomConcept(Concept):
+    Number = attr.ib(default=None)
+
+
+
+@attr.s
+class CustomLexeme(Lexeme):
+    Partial_Cognacy = attr.ib(default=None)
+    Borrowings = attr.ib(default=None)
+    Patterns = attr.ib(default=None)
+    Morpheme_Glosses = attr.ib(default=None)
+
+
+@attr.s
+class CustomLanguage(Language):
+    Latitude = attr.ib(default=None)
+    Longitude = attr.ib(default=None)
+    SubGroup = attr.ib(default=None)
+    Dataset = attr.ib(default=None)
+    Family = attr.ib(default=None)
+    Sources = attr.ib(default=None)
+
+
+class Dataset(BaseDataset):
+    dir = Path(__file__).parent
+    id = "chacolanguages"
+    concept_class = CustomConcept
+    language_class = CustomLanguage
+    lexeme_class = CustomLexeme
+    form_spec = FormSpec(
+            separators=',',
+            )
+
+    def cmd_download(self, args):
+        print('updating ...')
+        with open(self.raw_dir.joinpath("wordlist.tsv"), "w",
+                encoding="utf-8") as f:
+            f.write(fetch("chacolanguages"))
+
+    def cmd_makecldf(self, args):
+        args.writer.add_sources()
+
+        concepts = {}
+        for concept in self.concepts: #conceptlists[0].concepts.values():
+            idx = concept["NUMBER"].split("-")[-1] + "_" + slug(concept["ENGLISH"])
+            args.writer.add_concept(
+                ID=idx,
+                Name=concept["ENGLISH"],
+                Number=concept["NUMBER"],
+                #Concepticon_ID=concept.concepticon_id,
+                #Concepticon_Gloss=concept.concepticon_gloss,
+            )
+            concepts[concept["ENGLISH"]] = idx
+        args.writer.add_languages()
+        sources = {}
+        for lang in self.languages:
+            sources[lang["ID"]] = lang["Sources"].split(", ")
+
+        # we combine with the manually edited wordlist to retrieve the lexeme
+        # values
+        errors = set()
+        wl = Wordlist(self.raw_dir.joinpath('wordlist.tsv').as_posix())
+        for (
+                idx, lang, concept, val, form, tks, cogids, morphemes, borids,
+                patids
+                ) in wl.iter_rows(
+                        "doculect", "concept", "value", "form", "tokens",
+                        "cogids", "morphemes", "patids", "borids"
+                        ):
+            if concept.strip() in concepts:
+                erred = False
+                if not lang.strip():
+                    errors.add(("language", idx, lang))
+                    erred = True
+                if not concept.strip():
+                    errors.add(("concept", idx, concept))
+                    erred = True
+                if not tks:
+                    errors.add(("tokens", idx, lang+"-"+concept+"-"+" ".join(tks)))
+                    erred = True
+                if not erred:
+                    args.writer.add_form_with_segments(
+                                            Parameter_ID=concepts[concept.strip()],
+                                            Language_ID=lang,
+                                            Value=val.strip() or form.strip() or "?",
+                                            Form=form or val.strip() or "?",
+                                            Segments=tks,
+                                            Source="Deepadung2015",
+                                            Partial_Cognacy=" ".join([str(x) for x in
+                                                cogids]),
+                                            Morpheme_Glosses=morphemes,
+                                            Borrowings=borids,
+                                            Patterns=patids
+                                            )
+    
+        for row in sorted(errors):
+            print("{0:10} {1:10} {2:10}".format(*row))
+
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import json
+
+
+with open("metadata.json", encoding="utf-8") as fp:
+    metadata = json.load(fp)
+
+
+setup(
+    name="lexibank_chacolanguages",
+    version="1.0",
+    description=metadata["title"],
+    license=metadata.get("license", ""),
+    url=metadata.get("url", ""),
+    py_modules=["lexibank_chacolanguages"],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={"lexibank.dataset": ["chacolanguages=lexibank_chacolanguages:Dataset"]},
+    install_requires=["pylexibank>=3", "pyedictor>=0.1.2"],
+    extras_require={"test": ["pytest-cldf"]},
+)
